ff3d7e48426f77ac7e68a985e8734c8d0a35ea0c
vlnarability <adam.j.vlna.@gmail.com>
2021-04-14 17:32:28 -0400

    #6 Added Like Feature

--- finstagram/src/Post.js
+++ finstagram/src/Post.js
@@ -2,20 +2,64 @@ import React, { useState, useEffect } from "react";
 import "./Post.css";
 import { db } from "./firebase";
 import Avatar from "@material-ui/core/Avatar";
-import firebase from "firebase"
+import Tooltip from "@material-ui/core//Tooltip";
+import IconButton from "@material-ui/core/IconButton";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import FavoriteTwoToneIcon from "@material-ui/icons/FavoriteTwoTone";
+import firebase from "firebase";
 import Followers from "./Followers.js";
 import "./Followers.css";
 
 function Post({ postId, user, username, caption, imageUrl }) {
   const [comments, setComments] = useState([]);
   const [comment, setComment] = useState([]);
+  const [hover, setHover] = useState(false);
+  const [likes, setLike] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
+
+  const postRef = db.collection("posts").doc(postId);
+  const likeRef = postRef.collection("likes");
+
+  const likeRes = !user?.displayName
+    ? "Does Not Exist"
+    : likeRef.where("username", "==", user.displayName).get();
+
+  const like = (event) => {
+    likeRef.add({
+      username: user.displayName,
+    });
+    postRef.update({ likeCount: firebase.firestore.FieldValue.increment(1) });
+    setLike(true);
+  };
+
+  const unlike = (event) => {
+    likeRes.then((snapshot) => {
+      snapshot.forEach((doc) => {
+        doc.ref.delete();
+      });
+    });
+    postRef.update({ likeCount: firebase.firestore.FieldValue.increment(-1) });
+    setLike(false);
+  };
+
+  useEffect(() => {
+    if (likeRes !== "Does Not Exist") {
+      likeRes.then((snapshot) => {
+        if (snapshot.docs.length == 1 || likes) {
+          setLike(true);
+        } else {
+          setLike(false);
+        }
+      });
+    }
+    updateLikeCount();
+  },[]);
 
   useEffect(() => {
     let unsubscribe;
     if (postId) {
-      unsubscribe = db
-        .collection("posts")
-        .doc(postId)
+      unsubscribe = postRef
         .collection("comments")
         .orderBy("timestamp", "desc")
         .onSnapshot((snapshot) => {
@@ -30,16 +74,32 @@ function Post({ postId, user, username, caption, imageUrl }) {
 
   const postComment = (event) => {
     event.preventDefault();
-    db.collection("posts").doc(postId).collection("comments").add({
+    postRef.collection("comments").add({
       text: comment,
       username: user.displayName,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
     });
     setComment("");
   };
 
+  const updateLikeCount = () => {
+    postRef.get().then((snapshot) => {
+      if (snapshot.exists) {
+        postRef.onSnapshot((doc) => {
+          if (doc.data()["likeCount"] > 0) {
+            setLikeCount(doc.data()["likeCount"]);
+          } else {
+            setLikeCount(0);
+          }
+        });
+      } else {
+        setLikeCount(0);
+      }
+    });
+  };
+
   return (
-	<div className="Post">
+    <div className="Post">
       {/* header w/ avatar and Username*/}
       <div className="Post__header">
         <Avatar
@@ -48,7 +108,7 @@ function Post({ postId, user, username, caption, imageUrl }) {
           src="/static/images/avatar/1.png"
         />
         <p>
-          <h3>{username}</h3>
+          <strong>{username}</strong>
           {/* Get timestamp of each post*/}
           <span className="Post__timestamp"> Some Time Ago</span>
         </p>
@@ -59,21 +119,69 @@ function Post({ postId, user, username, caption, imageUrl }) {
       <h4 className="Post__text">
         <strong>{username}</strong> {caption}
       </h4>
+      {/* Like Button */}
+      <div className="Post__like">
+        {user?.displayName ? (
+          <Tooltip title={likeCount + " Like(s)"} arrow placement="right">
+            {likes ? (
+              <IconButton
+                className="Post__unlikeButton"
+                onMouseOver={() => setHover(true)}
+                onMouseOut={() => setHover(false)}
+                onClick={unlike}
+              >
+                {hover ? (
+                  <FavoriteTwoToneIcon
+                    style={{ color: "lightblue" }}
+                  ></FavoriteTwoToneIcon>
+                ) : (
+                  <FavoriteIcon style={{ color: "Crimson" }}></FavoriteIcon>
+                )}
+              </IconButton>
+            ) : (
+              <IconButton
+                className="Post__likeButton"
+                onMouseOver={() => setHover(true)}
+                onMouseOut={() => setHover(false)}
+                onClick={like}
+              >
+                {hover ? (
+                  <FavoriteTwoToneIcon
+                    style={{ color: "pink" }}
+                  ></FavoriteTwoToneIcon>
+                ) : (
+                  <FavoriteBorderIcon
+                    style={{ color: "Black" }}
+                  ></FavoriteBorderIcon>
+                )}
+              </IconButton>
+            )}
+          </Tooltip>
+        ) : (
+          <Tooltip title={likeCount + " Like(s)"} arrow placement="right">
+            <FavoriteTwoToneIcon
+              style={{ color: "lightgrey" }}
+            ></FavoriteTwoToneIcon>
+          </Tooltip>
+        )}
+      </div>
+
       {/* Comment Section */}
       <div className="Post__commentSection">
         {comments.map((comment) => (
           <p>
             {/* Get timestamp of each comment*/}
-            <strong>{comment.username}</strong> {comment.text} <br /> <span className="Post__timestamp"> Some Time Ago</span> 
+            <strong>{comment.username}</strong> {comment.text} <br />{" "}
+            <span className="Post__timestamp"> Some Time Ago</span>
           </p>
         ))}
       </div>
-	  {/* Following Button */}
-      <Followers 
-          //Current User
-          user={user}
-          //Username of Poster
-          username={username}
+      {/* Following Button */}
+      <Followers
+        //Current User
+        user={user}
+        //Username of Poster
+        username={username}
       />
       {/* Comment Box */}
       <form className="Post__commentBox">
